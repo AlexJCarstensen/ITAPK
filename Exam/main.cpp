e1424390aa4e0b01ef85eb72079d21ffbbff34b7
Alex Carstensen <matthacker.alex@gmail.com>
2016-10-09 13:47:06 +0200

    initial commit main.cpp and created header for pokemonstates

---
+++ Exam/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "pokemonState.h"
+
+
+using namespace pokemonGame;
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ Exam/pokemonState.h
@@ -0,0 +1,16 @@
+//
+// Created by plex on 10/9/16.
+//
+
+#ifndef EXAM_POKEMONSTATE_H
+#define EXAM_POKEMONSTATE_H
+
+
+namespace pokemonGame
+{
+
+
+}
+
+
+#endif //EXAM_POKEMONSTATE_H
